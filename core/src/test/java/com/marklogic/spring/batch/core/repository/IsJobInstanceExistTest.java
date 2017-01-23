f8f72d0ed168b3b02809883a764cdb7b8d2e976b
Scott Stafford <sstafford@marklogic.com>
2017-01-23 13:11:15 -0500

    Fix JobInstanceExists test

--- core/src/test/java/com/marklogic/spring/batch/core/repository/IsJobInstanceExistTest.java
+++ core/src/test/java/com/marklogic/spring/batch/core/repository/IsJobInstanceExistTest.java
@@ -1,44 +1,46 @@
 package com.marklogic.spring.batch.core.repository;
 
-import com.marklogic.spring.batch.AbstractSpringBatchTest;
+import com.marklogic.spring.batch.AbstractSpringBatchCoreTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
 
-public class IsJobInstanceExistTest extends AbstractSpringBatchTest {
+public class IsJobInstanceExistTest extends AbstractSpringBatchCoreTest {
 
     private final String jobName = "job";
     private JobParameters params;
+    private JobRepository jobRepository;
 
     @Before
     public void JobExecution() throws JobExecutionAlreadyRunningException, JobRestartException,
-        JobInstanceAlreadyCompleteException {
-        initializeJobRepository();
+        JobInstanceAlreadyCompleteException, Exception {
+        jobRepository = getMarklogicBatchConfigurer().getJobRepository();
         JobParametersBuilder builder = new JobParametersBuilder();
         builder.addLong("count", 123L);
-        getJobRepository().createJobExecution(jobName, builder.toJobParameters());
+        jobRepository.createJobExecution(jobName, builder.toJobParameters());
         params = builder.toJobParameters();
     }
 
     @Test
     public void verifyJobInstanceExistsTest() {
-        assertTrue(getJobRepository().isJobInstanceExists(jobName, params));
+        assertTrue(jobRepository.isJobInstanceExists(jobName, params));
     }
 
     @Test
     public void verifyJobInstanceDoesNotExistWithJobNameTest() {
-        assertFalse(getJobRepository().isJobInstanceExists(jobName + "-test", params));
+        assertFalse(jobRepository.isJobInstanceExists(jobName + "-test", params));
     }
 
     @Test
     public void verifyJobInstanceDoesNotExistWithJobParametersTest() {
         JobParametersBuilder builder = new JobParametersBuilder(params);
         builder.addLong("second", 100L, true);
-        assertFalse(getJobRepository().isJobInstanceExists(jobName, builder.toJobParameters()));
+        assertFalse(jobRepository.isJobInstanceExists(jobName, builder.toJobParameters()));
     }
 
 }
